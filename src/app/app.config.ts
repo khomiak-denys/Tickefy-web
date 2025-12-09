754fe22cab241f14cab4eb7e80988de10ddf1840
denyskhomiakpz2023 <denus.homiak@gmail.com>
2025-12-09 21:23:36 +0200

    refactor: deleted unused files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { testInterceptor } from './core/interceptors/test.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 import { routes } from './app.routes';
 
@@ -9,6 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([testInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
--- src/app/core/interceptors/auth.interceptor.spec.ts
+++ src/app/core/interceptors/auth.interceptor.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
 
-import { testInterceptor } from './test.interceptor';
+import { authInterceptor } from './auth.interceptor';
 
-describe('testInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => testInterceptor(req, next));
+describe('authInterceptor', () => {
+  const interceptor: HttpInterceptorFn = (req, next) =>
+    TestBed.runInInjectionContext(() => authInterceptor(req, next));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
