d82e1379fbcb00e1a0e246a2cdc60dcc4aa312e3
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-02-27 15:23:05 +0200

    fix: env variables

--- src/app/core/api/api.config.ts
+++ src/app/core/api/api.config.ts
@@ -1,2 +1,2 @@
 const env = (import.meta as { env?: Record<string, string> }).env || {};
-export const API_BASE_URL = env['NG_APP_API_BASE_URL'] ?? 'http://localhost:5000';
+export const API_BASE_URL = import.meta.env.NG_APP_API_BASE_URL;
