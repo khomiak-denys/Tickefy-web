70fa06fa45f6225e4aaf237badb7e5719e7e81d0
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-06-02 16:24:15 +0300

    refactor(logs): add dto, add typization to service

---
+++ src/app/core/api/dtos/activity-log.dto.ts
@@ -0,0 +1,10 @@
+import { UserShortDto } from "./user.dto";
+
+export interface ActivityLogDto {
+    id: string;
+    ticketId: string;
+    user: UserShortDto;
+    eventType: string;
+    description: string;
+    created: string;
+}
--- src/app/core/services/activity-log.service.ts
+++ src/app/core/services/activity-log.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { API_BASE_URL } from '../api/api.config';
+import {ActivityLogDto} from '../api/dtos/activity-log.dto';
 
 @Injectable({ providedIn: 'root' })
 export class ActivityLogService {
@@ -8,10 +9,10 @@ export class ActivityLogService {
 
   getLogs(page: number, pageSize: number) {
     const params = new HttpParams().set('Page', page).set('PageSize', pageSize);
-    return this.http.get(`${API_BASE_URL}/api/v1/logs`, { params });
+    return this.http.get<ActivityLogDto[]>(`${API_BASE_URL}/api/v1/logs`, { params });
   }
 
   getLogsByTicket(ticketId: string) {
-    return this.http.get(`${API_BASE_URL}/api/v1/logs/ticket/${ticketId}`);
+    return this.http.get<ActivityLogDto[]>(`${API_BASE_URL}/api/v1/logs/ticket/${ticketId}`);
   }
 }
