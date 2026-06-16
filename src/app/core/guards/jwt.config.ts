cf06411bed07ae5cf02d9e5177a975af467be994
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-06-16 16:14:19 +0300

    refactor(env-import): add import.meta alias

--- src/app/core/guards/jwt.config.ts
+++ src/app/core/guards/jwt.config.ts
@@ -1,2 +1,4 @@
-export const JWT_ISSUER = import.meta.env.NG_APP_JWT_ISSUER ?? 'Tickefy.API';
-export const JWT_AUDIENCE = import.meta.env.NG_APP_JWT_AUDIENCE ?? 'Tickefy.Client';
+import { rawImportMetaEnv } from '../config/import-meta-env';
+
+export const JWT_ISSUER = rawImportMetaEnv.NG_APP_JWT_ISSUER ?? 'Tickefy.API';
+export const JWT_AUDIENCE = rawImportMetaEnv.NG_APP_JWT_AUDIENCE ?? 'Tickefy.Client';
