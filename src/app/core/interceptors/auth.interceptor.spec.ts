7a89fa21b2b062c68d4e14c9c5f0fea1c783c025
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-06-16 16:22:29 +0300

    refactor(auth-interceptor): replace direct localStorage logic with service

--- src/app/core/interceptors/auth.interceptor.spec.ts
+++ src/app/core/interceptors/auth.interceptor.spec.ts
@@ -1,17 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { HttpInterceptorFn, HttpRequest, provideHttpClient } from '@angular/common/http';
 import { authInterceptor } from './auth.interceptor';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AuthService } from '../services/auth.service';
+
+jest.mock('../config/env.config', () => ({
+  appEnv: {
+    NG_APP_API_BASE_URL: 'http://localhost:5000',
+    NG_APP_JWT_ISSUER: 'tickefy',
+    NG_APP_JWT_AUDIENCE: 'tickefy-web',
+  },
+}));
 
 describe('authInterceptor', () => {
   const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => authInterceptor(req, next));
 
+  let authService: AuthService;
+  let getTokenSpy: any;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
+
+    authService = TestBed.inject(AuthService);
+    getTokenSpy = jest.spyOn(authService, 'getAccessToken').mockReturnValue('fake-token');
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  it('Token exists', () => {
+    const req = new HttpRequest('GET', '/auth');
+    const next = jest.fn();
+    interceptor(req, next);
+    const clonedRequest = next.mock.calls[0][0];
+
+    expect(clonedRequest.headers.get('Authorization')).toEqual('Bearer fake-token');
+  });
+
+  it('Token not exists', () => {
+    const req = new HttpRequest('GET', '/auth');
+    const next = jest.fn();
+    getTokenSpy.mockReturnValue('');
+    interceptor(req, next);
+    const clonedRequest = next.mock.calls[0][0];
+
+    expect(clonedRequest).toEqual(req);
   });
 });
--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,8 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
-// Attaches JWT access token from localStorage to outgoing requests
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('access_token');
+  const service = inject(AuthService);
+  const token = service.getAccessToken();
   const authReq = token ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : req;
   return next(authReq);
 };
