e18c184fb43a5a0f9e41bfd815738c52a3ca10e7
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-07-02 10:03:03 +0300

    fix(auth-interceptor): add retry on Unathorized request

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,10 +1,27 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpStatusCode } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const service = inject(AuthService);
   const token = service.getAccessToken();
   const authReq = token ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) : req;
-  return next(authReq);
+
+  return next(authReq).pipe(
+    catchError((error) => {
+      if (error.status === HttpStatusCode.Unauthorized) {
+        return service.refreshToken().pipe(
+          switchMap((result) => {
+            service.saveToken(result.token);
+            const refreshedReq = req.clone({
+              setHeaders: { Authorization: `Bearer ${result.token}` },
+            });
+            return next(refreshedReq);
+          })
+        );
+      }
+      return throwError(() => error);
+    })
+  );
 };
