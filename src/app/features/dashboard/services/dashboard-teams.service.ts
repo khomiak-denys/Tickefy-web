c71b8b5a33e9d49ded2803ae02c670385a1c324e
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-07-05 22:21:48 +0300

    feat(team-details): add caching to service, use dashboardTeams service instead of repo

--- src/app/features/dashboard/services/dashboard-teams.service.ts
+++ src/app/features/dashboard/services/dashboard-teams.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { TeamsService } from '../../../core/services/teams.service';
-import { BehaviorSubject, tap } from 'rxjs';
-import { CreateTeamRequest, TeamSummary } from '../../../core/api/dtos';
+import { BehaviorSubject, Observable, of, tap } from 'rxjs';
+import { CreateTeamRequest, TeamDetails, TeamSummary } from '../../../core/api/dtos';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +16,8 @@ export class DashboardTeamsService {
     private auth: AuthService,
     private teams: TeamsService
   ) {}
+
+  private _cache: Map<string, Observable<TeamDetails>> = new Map();
 
   loadTeams() {
     const role = this.auth.getRole();
@@ -46,5 +49,33 @@ export class DashboardTeamsService {
 
   createTeam(req: CreateTeamRequest) {
     return this.teams.create(req).pipe(tap(() => this.loadTeams()));
+  }
+
+  getById(id: string) {
+    const observable = this._cache.get(id);
+
+    if (!observable) {
+      const request = this.teams.getById(id).pipe(shareReplay(1));
+      this._cache.set(id, request);
+      return request;
+    }
+
+    return observable;
+  }
+
+  addMemberByLogin(teamId: string, login: string) {
+    return this.teams
+      .addMemberByLogin(teamId, login)
+      .pipe(tap(() => this.invalidateTeamDetails(teamId)));
+  }
+
+  removeMember(teamId: string, memberId: string) {
+    return this.teams
+      .removeMember(teamId, memberId)
+      .pipe(tap(() => this.invalidateTeamDetails(teamId)));
+  }
+
+  private invalidateTeamDetails(teamId: string) {
+    this._cache.delete(teamId);
   }
 }
