d162e2b1a0b9cefb2c507afc4093c164652e4822
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-07-13 01:05:08 +0300

    fix(dashboard-users-service): add tests for delete method

--- src/app/features/dashboard/services/dashboard-user.service.spec.ts
+++ src/app/features/dashboard/services/dashboard-user.service.spec.ts
@@ -1,9 +1,11 @@
 import { DashboardUserService } from './dashboard-user.service';
 import { AuthService } from '../../../core/services/auth.service';
 import {UsersService} from '../../../core/services/users.service';
-import {Observable, of, throwError} from 'rxjs';
-import {TeamSummary, UserDto, UserShortDto} from '../../../core/api/dtos';
+import {of, throwError} from 'rxjs';
+import {TeamSummary, UserDto} from '../../../core/api/dtos';
 
+
+const NETWORK_ERROR = 'Network Error';
 describe('DashboardUserService', () => {
   let service: DashboardUserService;
   let mockAuth: jest.Mocked<Pick<AuthService, 'getRole'>>;
@@ -96,7 +98,7 @@ describe('DashboardUserService', () => {
 
     it ( 'error$ should get value on http failure', () => {
       mockAuth.getRole.mockReturnValue('admin');
-      mockUsers.getAll.mockReturnValue(throwError(() => 'Network error'));
+      mockUsers.getAll.mockReturnValue(throwError(() => NETWORK_ERROR));
 
       let errorValue: string | null = null;
       const sub = service.userError$.subscribe(err => {
@@ -105,7 +107,40 @@ describe('DashboardUserService', () => {
 
       service.loadUsers();
 
-      expect(errorValue).toBe('Network error');
+      expect(errorValue).toBe(NETWORK_ERROR);
     });
   });
+
+  describe('deleteUser()', () => {
+
+    it('delete should not be called when id is null', () => {
+      service.deleteUser(null as any);
+
+      expect(mockUsers.delete).not.toHaveBeenCalled();
+    })
+
+    it('should invalidate cache on successful delete', () => {
+      mockUsers.delete.mockReturnValue(of(void 0));
+      mockUsers.getAll.mockReturnValue(of([]));
+      mockAuth.getRole.mockReturnValue('admin');
+
+      service.deleteUser('1');
+
+      expect(mockUsers.delete).toHaveBeenCalledTimes(1);
+      expect(mockUsers.getAll).toHaveBeenCalledTimes(1);
+    });
+
+    it ('should push error to stream on http failure', () => {
+      mockUsers.delete.mockReturnValue(throwError(() => NETWORK_ERROR));
+
+      let errorValue: string | null = null;
+      const sub = service.userError$.subscribe(err => {
+        errorValue = err;
+      });
+
+      service.deleteUser('1');
+
+      expect(errorValue).toBe(NETWORK_ERROR);
+    })
+  })
 });
