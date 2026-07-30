2888c64926e6e9a14d68ed9a6ecd8cd1b9379cab
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-07-30 20:13:31 +0300

    feat(queue-tab): add component test

--- src/app/features/dashboard/components/queue-tab/queue-tab.component.spec.ts
+++ src/app/features/dashboard/components/queue-tab/queue-tab.component.spec.ts
@@ -1,22 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { QueueTabComponent } from './queue-tab.component';
 
 describe('QueueTabComponent', () => {
   let component: QueueTabComponent;
-  let fixture: ComponentFixture<QueueTabComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [QueueTabComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(QueueTabComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new QueueTabComponent();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should emit acceptedTicketId when onAcceptClick is called', () => {
+    const ticketId = '1';
+    const emitSpy = jest.spyOn(component.acceptedTicketId, 'emit');
+
+    component.onAcceptClick(ticketId);
+
+    expect(emitSpy).toHaveBeenCalledWith(ticketId);
   });
 });
