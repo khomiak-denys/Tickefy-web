54e92da10c01074f942820eb6f01471907dc5f06
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-08-04 21:36:59 +0300

    feat(notifications): add service & dto

---
+++ src/app/shared/services/notification-service.service.spec.ts
@@ -0,0 +1,113 @@
+import { Notification } from '../helpers/dto/notification.dto';
+import { NotificationServiceService } from './notification-service.service';
+
+describe('NotificationServiceService', () => {
+  let service: NotificationServiceService;
+
+  beforeEach(() => {
+    jest.useFakeTimers();
+    service = new NotificationServiceService();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('should be three notifications after add three times', () => {
+    //Arrange
+    service.info('one');
+    service.error('two');
+    service.info('three');
+
+    //Act
+    let result: Notification[] = [];
+    service.notifications$.subscribe((v) => (result = v));
+
+    //Assert
+    expect(result.length).toBe(3);
+    expect(result[0].type).toBe('info');
+    expect(result[1].type).toBe('error');
+    expect(result[2].type).toBe('info');
+
+    expect(result[0].message).toBe('one');
+    expect(result[1].message).toBe('two');
+    expect(result[2].message).toBe('three');
+  });
+
+  it('should not be more than 3 notifications at the same time', () => {
+    //Arrange
+    service.info('one');
+    service.error('two');
+    service.info('three');
+    service.error('four');
+
+    //Act
+    let result: Notification[] = [];
+    service.notifications$.subscribe((v) => (result = v));
+
+    //Assert
+    expect(result.length).toBe(3);
+    expect(result[0].type).toBe('info');
+    expect(result[1].type).toBe('error');
+    expect(result[2].type).toBe('info');
+
+    expect(result[0].message).toBe('one');
+    expect(result[1].message).toBe('two');
+    expect(result[2].message).toBe('three');
+  });
+
+  it('should be three notifications after add three times and remove last', () => {
+    //Arrange
+    service.info('one');
+    service.error('two');
+    service.info('three');
+    service.error('four');
+
+    //Act 1
+    let result: Notification[] = [];
+    service.notifications$.subscribe((v) => (result = v));
+
+    //Assert 1
+    expect(result.length).toBe(3);
+    expect(result[0].type).toBe('info');
+    expect(result[1].type).toBe('error');
+    expect(result[2].type).toBe('info');
+
+    expect(result[0].message).toBe('one');
+    expect(result[1].message).toBe('two');
+    expect(result[2].message).toBe('three');
+
+    //Act 2
+    service.dismiss(result[0].id);
+
+    //Assert
+    expect(result.length).toBe(3);
+    expect(result[0].type).toBe('error');
+    expect(result[1].type).toBe('info');
+    expect(result[2].type).toBe('error');
+
+    expect(result[0].message).toBe('two');
+    expect(result[1].message).toBe('three');
+    expect(result[2].message).toBe('four');
+  });
+
+  it('should remove notification after 5 seconds', () => {
+    //Arrange
+    service.info('one');
+
+    //Act 1
+    let result: Notification[] = [];
+    service.notifications$.subscribe((v) => (result = v));
+
+    //Assert 1
+    expect(result.length).toBe(1);
+    expect(result[0].type).toBe('info');
+    expect(result[0].message).toBe('one');
+
+    //Act 2
+    jest.advanceTimersByTime(5000);
+
+    //Assert 2
+    expect(result.length).toBe(0);
+  });
+});
---
+++ src/app/shared/services/notification-service.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Notification } from '../helpers/dto/notification.dto';
+import { BehaviorSubject } from 'rxjs';
+import { randomUUID } from 'crypto';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationServiceService {
+  private _notificationsQueue: Notification[] = [];
+  private _notifications: BehaviorSubject<Notification[]> = new BehaviorSubject<Notification[]>([]);
+  readonly notifications$ = this._notifications.asObservable();
+
+  constructor() {}
+
+  info(message: string) {
+    this.createNotification(message, 'info');
+  }
+
+  error(message: string) {
+    this.createNotification(message, 'error');
+  }
+
+  dismiss(id: string) {
+    this.remove(id);
+  }
+
+  private createNotification(message: string, type: 'info' | 'error') {
+    const notification: Notification = { id: randomUUID(), message, type };
+
+    if (this._notifications.value.length < 3) {
+      this._notifications.next([...this._notifications.value, notification]);
+      this.activateNotification(notification);
+    } else {
+      this._notificationsQueue.push(notification);
+    }
+  }
+
+  private remove(id: string) {
+    const remaintNotifications = this._notifications.value.filter((n) => n.id !== id);
+
+    if (this._notificationsQueue.length !== 0) {
+      const pendingNotification = this._notificationsQueue.shift();
+      this._notifications.next([...remaintNotifications, pendingNotification!]);
+      this.activateNotification(pendingNotification!);
+    } else {
+      this._notifications.next(remaintNotifications);
+    }
+  }
+
+  private activateNotification(notification: Notification) {
+    setTimeout(() => {
+      this.remove(notification.id);
+    }, 5000);
+  }
+}
