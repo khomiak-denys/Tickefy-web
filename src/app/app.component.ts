3b8b181879cf010d2c03fcb942aaf71a902f7cba
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-08-05 01:01:25 +0300

    feat(notifications): integrate with app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotificationHostComponent } from './shared/components/notification-host/notification-host.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotificationHostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
