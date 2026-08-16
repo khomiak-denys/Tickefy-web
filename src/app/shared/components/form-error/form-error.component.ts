8b14cfe8175b990ea4eaa7b1138dc142718858d3
denyskhomiakpz2023 <denus.homiak@gmail.com>
2026-08-16 18:17:12 +0300

    feat(errors): add form error component

---
+++ src/app/shared/components/form-error/form-error.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-error',
+  standalone: true,
+  templateUrl: './form-error.component.html',
+  styleUrl: './form-error.component.scss',
+})
+export class FormErrorComponent {
+  @Input() control: AbstractControl | null = null;
+}
